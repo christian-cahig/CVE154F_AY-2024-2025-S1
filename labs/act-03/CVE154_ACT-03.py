8c8a4970ba2cc554bef8d900dc3be38c93b22e7d
Christian Cahig <christian.cahig@outlook.com>
2024-12-09 11:48:56 +0800

    Add Lab Activity 3

---
+++ labs/act-03/CVE154_ACT-03.py
@@ -0,0 +1,17 @@
+# MSU - Iligan Institute of Technology
+# CVE154 A.Y. 2024-2025 S1
+# Exam 3 and Lab Activity 3
+# 
+# A group effort by
+#   Write name of Member here
+#   Write name of Member here
+
+# Replace the `pass` statement with necessary imports
+import math as mt
+
+# Replace the `pass` statement with definitions of f and J
+pass
+
+if __name__ == "__main__":
+    # Replace the `pass` statement with appropriate lines of code
+    pass
